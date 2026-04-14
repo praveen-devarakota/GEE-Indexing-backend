4576b8fbb41b628eeb3b5a31200d3664fc38eca1
praveen <238w1a1285@vrsec.ac.in>
2026-04-14 19:42:27 +0530

    Initialize RAG pipeline structure

--- app.py
+++ app.py
@@ -2,22 +2,26 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from config.gee import init_earth_engine
 from routes.satellite_routes import satellite_bp
+import os
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)  # global CORS
+    CORS(app)
+
+    print("API KEY:", os.getenv("GROQ_API_KEY"))
 
     init_earth_engine()
+
     app.register_blueprint(satellite_bp)
 
-    # ✅ Index Route
     @app.route("/", methods=["GET"])
     def index():
         return jsonify({
             "status": "success",
             "message": "Satellite API is running 🚀",
             "available_routes": [
-                "/api/satellite/..."
+                "/api/satellite/...",
+                "/api/analyze"
             ]
         })
 
